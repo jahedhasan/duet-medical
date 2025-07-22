7846d4c9ff5440d8797e8dd81374537a8a249741
jahedhasan <jahedhasan.bs@gmail.com>
2025-07-22 22:27:21 +0600

    update DB connection

--- employee/includes/dbconnection.php
+++ employee/includes/dbconnection.php
@@ -3,7 +3,7 @@
 define('DB_HOST','localhost');
 define('DB_USER','root');
 define('DB_PASS','');
-define('DB_NAME','duet-medical');
+define('DB_NAME','duet_medical');
 // Establish database connection.
 try
 {
